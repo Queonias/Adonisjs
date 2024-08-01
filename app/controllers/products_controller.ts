0a2dd25dcaaada888aacda9a99aacc294537a4ed
queonias <queones@gmail.com>
2024-08-01 01:41:22 -0300

    feat: rota para deletar produto criada com logica soft delete

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -11,7 +11,7 @@ export default class ProductsController {
    */
   async index({ response }: HttpContext) {
     try {
-      const products = await Product.query().select('id', 'name', 'price').orderBy('name', 'asc')
+      const products = await Product.query().where('is_deleted', false).select('id', 'name', 'price').orderBy('name', 'asc')
       return response.status(200).json(products)
     } catch (error) {
       return response.status(400).json({ message: 'Error fetching products', error })
@@ -27,14 +27,22 @@ export default class ProductsController {
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) { }
+  async store({ request, response }: HttpContext) {
+    try {
+      const data = request.only(['name', 'price', 'description'])
+      const product = await Product.create(data)
+      return response.status(201).json(product)
+    } catch (error) {
+      return response.status(400).json({ message: 'Error creating product', error })
+    }
+  }
 
   /**
    * Show individual record
    */
   async show({ params, response }: HttpContext) {
     try {
-      const product = await Product.findOrFail(params.id)
+      const product = await Product.query().where('id', params.id).andWhere('is_deleted', false).firstOrFail()
       return response.status(200).json(product)
     } catch (error) {
       return response.status(404).json({ message: 'Product not found', error })
@@ -49,10 +57,31 @@ export default class ProductsController {
   /**
    * Handle form submission for the edit action
    */
-  // async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      const data = request.only(['name', 'price', 'description'])
+
+      product.merge(data)
+      await product.save()
+
+      return response.status(200).json(product)
+    } catch (error) {
+      return response.status(400).json({ message: 'Error updating product', error })
+    }
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) { }
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      product.is_deleted = true
+      await product.save()
+      return response.status(200).json({ message: 'Product deleted successfully' })
+    } catch (error) {
+      return response.status(400).json({ message: 'Error deleting product', error })
+    }
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -16,3 +16,6 @@ router.delete('/api/clients/:id', [ClientsController, 'destroy']);
 
 router.get('/api/products/list', [ProductsController, 'index']);
 router.get('/api/products/:id', [ProductsController, 'show']);
+router.post('/api/products/save', [ProductsController, 'store']);
+router.put('/api/products/:id', [ProductsController, 'update']);
+router.delete('/api/products/:id', [ProductsController, 'destroy']);
