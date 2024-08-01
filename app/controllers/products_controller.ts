ff8d9b3617ddef2a2669cadcc49fa1bd07d8afe6
queonias <queones@gmail.com>
2024-08-01 01:50:00 -0300

    fix: types

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,17 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import Client from '../models/client.js'
-import Address from '../models/address.js'
-import Phone from '../models/phone.js'
-import Sale from '../models/sale.js'
 import Product from '../models/product.js'
 
+
 export default class ProductsController {
-  /**
-   * Display a list of resource
-   */
+  // List all products
   async index({ response }: HttpContext) {
     try {
-      const products = await Product.query().where('is_deleted', false).select('id', 'name', 'price').orderBy('name', 'asc')
+      const products: Product[] = await Product.query().where('is_deleted', false).select('id', 'name', 'price').orderBy('name', 'asc')
       return response.status(200).json(products)
     } catch (error) {
       return response.status(400).json({ message: 'Error fetching products', error })
@@ -19,47 +14,31 @@ export default class ProductsController {
   }
 
 
-  /**
-   * Display form to create a new record
-   */
-  async create({ }: HttpContext) { }
-
-  /**
-   * Handle form submission for the create action
-   */
   async store({ request, response }: HttpContext) {
+    // Create a new product
     try {
       const data = request.only(['name', 'price', 'description'])
-      const product = await Product.create(data)
+      const product: Product = await Product.create(data)
       return response.status(201).json(product)
     } catch (error) {
       return response.status(400).json({ message: 'Error creating product', error })
     }
   }
 
-  /**
-   * Show individual record
-   */
   async show({ params, response }: HttpContext) {
+    // Show a product
     try {
-      const product = await Product.query().where('id', params.id).andWhere('is_deleted', false).firstOrFail()
+      const product: Product = await Product.query().where('id', params.id).andWhere('is_deleted', false).firstOrFail()
       return response.status(200).json(product)
     } catch (error) {
       return response.status(404).json({ message: 'Product not found', error })
     }
   }
 
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) { }
-
-  /**
-   * Handle form submission for the edit action
-   */
   async update({ params, request, response }: HttpContext) {
+    // Update a product
     try {
-      const product = await Product.findOrFail(params.id)
+      const product: Product = await Product.findOrFail(params.id)
       const data = request.only(['name', 'price', 'description'])
 
       product.merge(data)
@@ -71,12 +50,10 @@ export default class ProductsController {
     }
   }
 
-  /**
-   * Delete record
-   */
   async destroy({ params, response }: HttpContext) {
+    // Delete a product
     try {
-      const product = await Product.findOrFail(params.id)
+      const product: Product = await Product.findOrFail(params.id)
       product.is_deleted = true
       await product.save()
       return response.status(200).json({ message: 'Product deleted successfully' })
