92a08e4e1cd85820a104d0099a8042ad994f5bf7
queonias <queones@gmail.com>
2024-08-03 17:05:45 -0300

    feat: rotas protegidas pelo middleware

--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,5 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 const UsersController = () => import('#controllers/users_controller')
 const ClientsController = () => import('#controllers/clients_controller')
 const ProductsController = () => import('#controllers/products_controller')
@@ -7,16 +8,16 @@ const SalesController = () => import('#controllers/sales_controller')
 router.post('/api/users/signup', [UsersController, 'signup']);
 router.post('/api/users/login', [UsersController, 'login']);
 
-router.get('/api/clients/list', [ClientsController, 'index']);
-router.post('/api/clients/save', [ClientsController, 'store']);
-router.get('/api/clients/:id', [ClientsController, 'show']);
-router.put('/api/clients/:id', [ClientsController, 'update']);
-router.delete('/api/clients/:id', [ClientsController, 'destroy']);
+router.get('/api/clients/list', [ClientsController, 'index']).use(middleware.auth());
+router.post('/api/clients/save', [ClientsController, 'store']).use(middleware.auth());
+router.get('/api/clients/:id', [ClientsController, 'show']).use(middleware.auth());
+router.put('/api/clients/:id', [ClientsController, 'update']).use(middleware.auth());
+router.delete('/api/clients/:id', [ClientsController, 'destroy']).use(middleware.auth());
 
-router.get('/api/products/list', [ProductsController, 'index']);
-router.get('/api/products/:id', [ProductsController, 'show']);
-router.post('/api/products/save', [ProductsController, 'store']);
-router.put('/api/products/:id', [ProductsController, 'update']);
-router.delete('/api/products/:id', [ProductsController, 'destroy']);
+router.get('/api/products/list', [ProductsController, 'index']).use(middleware.auth());
+router.get('/api/products/:id', [ProductsController, 'show']).use(middleware.auth());
+router.post('/api/products/save', [ProductsController, 'store']).use(middleware.auth());
+router.put('/api/products/:id', [ProductsController, 'update']).use(middleware.auth());
+router.delete('/api/products/:id', [ProductsController, 'destroy']).use(middleware.auth());
 
-router.post('/api/sales/save', [SalesController, 'store']);
+router.post('/api/sales/save', [SalesController, 'store']).use(middleware.auth());
